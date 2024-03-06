81e46da4a128fe1e7d5ecb9430fc88e8a1f56305
Teodor.Popa <teodor.popa@mrm.com>
2024-03-06 10:31:38 +0200

    Centered output

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+const SEPARATOR = "   "
+
+func rowLength(row []int64) int {
+	length := 0
+	for _, num := range row {
+		length += len(fmt.Sprintf("%d", num)) + len(SEPARATOR)
+	}
+	length -= len(SEPARATOR)
+
+	return length
+}
 
 func pascalTriangle(rows *int64) {
+	var rowsData = make([][]int64, *rows)
 	var row, col int64
 	var number int64
 
@@ -12,14 +29,28 @@ func pascalTriangle(rows *int64) {
 			if col > 0 {
 				number = number * (row - col + 1) / col
 			}
-			print(number, " ")
+			rowsData[row] = append(rowsData[row], number)
 		}
-		println()
 	}
+
+	maxWidth := rowLength(rowsData[*rows-1])
+
+	for _, r := range rowsData {
+		var padding = (maxWidth - rowLength(r)) / 2
+
+		fmt.Print(strings.Repeat(" ", int(padding)))
+		for _, num := range r {
+			fmt.Print(num, SEPARATOR)
+		}
+
+		fmt.Println()
+	}
+
 }
 
 func main() {
 	var rows = flag.Int64("rows", 5, "Number of rows in Pascal's Triangle")
+	flag.Parse()
 
 	pascalTriangle(rows)
 }
